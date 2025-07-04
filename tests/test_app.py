30813be55972e155126d1c5167c88c775378af3d
CarlosNunek <chnunez@uce.edu.ec>
2025-07-03 21:31:56 -0500

    fix: corregido campos en test v8

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,12 +6,21 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import app
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
-@patch("config.mongo.mongo.db.evaluaciones_psicologicas.insert_one")
-def test_ingresar_evaluacion_exito(mock_insert, client):
-    mock_insert.return_value.inserted_id = "fake_id_123"
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
 
+@pytest.fixture
+def mock_insert_one():
+    with patch("config.mongo.mongo.db.evaluaciones_psicologicas.insert_one") as mock_insert:
+        mock_insert.return_value.inserted_id = "fake_id_123"
+        yield mock_insert
+
+def test_ingresar_evaluacion_exito(client, mock_insert_one):
     payload = {
         "id_recluso": "1725279812",
         "nivel_agresividad": 4,
@@ -24,7 +33,7 @@ def test_ingresar_evaluacion_exito(mock_insert, client):
         "observaciones": "Requiere tratamiento"
     }
 
-    response = client.post("/api/ingresar_evaluacion", json=payload)  # ✅ más seguro
+    response = client.post("/api/ingresar_evaluacion", json=payload)
     print("STATUS:", response.status_code)
     print("RESPONSE:", response.get_json())
 
